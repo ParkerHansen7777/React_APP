4d72020a6a2bb5a458c297def3efeb0e9cf65282
ParkerHansen7777 <parkerhansen7777@gmail.com>
2021-02-26 13:46:34 -0600

    Clean Slate fix

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
 import React from 'react';
-import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div class="topnav">
-          <a class="active" href="#home">Home</a>
-          <a href="#news">News</a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About</a>
-        </div>
-        <div className="Main_Logo">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-      </header>
-    <main className="App-main">
-    <a
-          className="App-link"
-          href="https://tracker.gg/valorant/profile/riot/Pramado%236946/overview"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Valorant
-        </a>
-        <a
-          className="App-link"
-          href="https://cod.tracker.gg/warzone/profile/battlenet/Pramado%231231/overview"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Warzone
-        </a>
-		<a
-          className="App-link"
-          href="https://r6.tracker.network/profile/pc/Pramado"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Rainbow Six Siege 6
-        </a>
-    </main>
+    <div>
     </div>
   );
 }
