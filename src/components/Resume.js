fd80ae21fa0faeb9ccf7564d5939083c9777ee2d
ParkerHansen7777 <parkerhansen7777@gmail.com>
2021-03-04 12:57:07 -0600

    update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Resume.css';
-
-
+import { Document, Page } from 'react-pdf';
+import pdf from './ParkerHansenResume.pdf';
 class Resume extends React.Component{
-render(){
+
+    render(){
     return(
     
     <   div>
             <body>
                 <h1>Welcome to my Resume</h1>
+                
+                
             </body>
         </div>
     
