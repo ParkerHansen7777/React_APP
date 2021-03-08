a634e1d0770ec2385cdd4917c43d48e10a720223
ParkerHansen7777 <parkerhansen7777@gmail.com>
2021-03-08 13:00:52 -0600

    PDF viewer working
    Added Resume PDF to resume page.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Resume.css';
+import SinglePage from "./SinglePage.js";
+import samplePDF from "./ParkerHansenResume.pdf";
+
+
 class Resume extends React.Component{
 
     render(){
     return(
     
-    <   div class="head2">
+    <div class="head2">
             <body>
                 <h1>Welcome to my Resume</h1>
-                
-                
+                <SinglePage pdf={samplePDF} />
+
+                <hr />
+            
             </body>
         </div>
-    
-    
-    
      );
 
 
