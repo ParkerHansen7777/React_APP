129d5d52952ffa8a74c1dcd5d3eed688efd671bb
ParkerHansen7777 <parkerhansen7777@gmail.com>
2021-03-09 16:57:08 -0600

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Resume.css';
 import SinglePage from "./SinglePage.js";
-import samplePDF from "./ParkerHansenResume.pdf";
+import resumePDF from "./ParkerHansenResume.pdf";
 
 
 class Resume extends React.Component{
@@ -12,10 +12,7 @@ class Resume extends React.Component{
     <div class="head2">
             
                 <h1>Welcome to my Resume</h1>
-                <SinglePage pdf={samplePDF} />
-
-                <hr />
-            
+                <SinglePage pdf={resumePDF} />
             
         </div>
      );
