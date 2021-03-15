138b267aee83fb5802a4ed3391de0e9f9bcc502c
ParkerHansen7777 <parkerhansen7777@gmail.com>
2021-03-15 11:46:18 -0500

    Created Better UI for Portfolio page.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,7 +8,8 @@ render(){
     
     <div class = "head1">
                 <h1>Welcome to my Portfolio</h1>
-                <a href = "https://github.com/ParkerHansen7777/Portfolio">GitHub Portfolio</a>
+                <button class="button1"><a href = "https://github.com/ParkerHansen7777/Portfolio" target="_blank">GitHub Portfolio</a></button>
+                <button class="button2"><a href = "https://github.com/ParkerHansen7777/React_APP" target="_blank">React App Repo</a></button>
     </div>
     
      );
