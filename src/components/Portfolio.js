b7bc74dd5bbbb150395fd01c438944c2cc26c3b5
ParkerHansen7777 <parkerhansen7777@gmail.com>
2021-03-15 18:37:41 -0500

    Updated UI

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,6 +10,7 @@ render(){
                 <h1>Welcome to my Portfolio</h1>
                 <button class="button1"><a href = "https://github.com/ParkerHansen7777/Portfolio" target="_blank">GitHub Portfolio</a></button>
                 <button class="button2"><a href = "https://github.com/ParkerHansen7777/React_APP" target="_blank">React App Repo</a></button>
+                <button class="button3"><a href = "https://www.linkedin.com/in/parkerhansen/" target="_blank">LinkedIn</a></button>
     </div>
     
      );
