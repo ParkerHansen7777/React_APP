70e65a929c3250fbf59f91d3aaaa69d2b34d1653
ParkerHansen7777 <parkerhansen7777@gmail.com>
2024-06-18 11:54:58 -0700

    Project refinement

---
+++ src/components/Home/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Home.css';
+import profile from './headshot.jpg';
+
+
+class Home extends React.Component{
+render(){
+    return(
+    
+        <div class='p-body'>
+                <div class='flex-item'>
+                    <h1>Hello, my name is Parker Hansen</h1>
+                    <br/>
+                </div>
+                
+                <div class='flex-item'>              
+                    <img src ={profile} alt="of Parker"/>
+                    <p>I'm a software developer based in Los Angeles, California.</p>
+                    <button class="button1"><a href = "https://github.com/ParkerHansen7777?tab=repositories" target="_blank" rel="noreferrer">GitHub</a></button>
+                    <button class="button2"><a href = "https://github.com/ParkerHansen7777/React_APP" target="_blank" rel="noreferrer">React App Repo</a></button>
+                    <button class="button3"><a href = "https://www.linkedin.com/in/parkerhansen/" target="_blank"rel="noreferrer">LinkedIn</a></button>
+                </div>
+                
+                <div class='flex-item'>
+                    <h2 class="links">Links to my other projects:</h2>
+                    <a href = "https://cardealer-frontend.onrender.com">Luxury Autos Car Dealership</a>
+                    <a href = "https://taskmanager-frontend-8dip.onrender.com">Task Manager</a>
+                    <a href = "https://gametracker-frontend.onrender.com">Game Tracker</a>
+                </div>  
+                
+                <footer className="Page-footer"><span>Created by Parker (© 2021)</span></footer>
+        </div>
+    
+    );
+ }   
+}
+
+export default Home
