3b88dd5fd23d4c3fb76711fdf0c38970da083381
ParkerHansen7777 <parkerhansen7777@gmail.com>
2024-06-19 10:55:25 -0700

    Page updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import profile from './headshot.jpg';
+import profile from './linkedinPfp.jpg';
 
 
 class Home extends React.Component{
@@ -10,12 +10,14 @@ render(){
         <div class='p-body'>
                 <div class='flex-item'>
                     <h1>Hello, my name is Parker Hansen</h1>
-                    <br/>
+                    
                 </div>
                 
                 <div class='flex-item'>              
                     <img src ={profile} alt="of Parker"/>
-                    <p>I'm a software developer based in Los Angeles, California. I love to problem solve.</p>
+                    <p>I'm a software developer based in Los Angeles, California. I have experience with languages and frameworks such as, C, C++, Python, JavaScript, Java, React.JS, Node.JS, SQL. 
+                        I currently freelancing, training AI with a focus on software development. I've created multiple projects React with some being full stack, i've even made a minecraft mod with Java!. 
+                        I love to problem solve. </p>
                     <button class="button1"><a href = "https://github.com/ParkerHansen7777?tab=repositories" target="_blank" rel="noreferrer">GitHub</a></button>
                     <button class="button2"><a href = "https://github.com/ParkerHansen7777/React_APP" target="_blank" rel="noreferrer">Portfolio App Repo</a></button>
                     <button class="button3"><a href = "https://www.linkedin.com/in/parkerhansen/" target="_blank"rel="noreferrer">LinkedIn</a></button>
