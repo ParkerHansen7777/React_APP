c24039a0eec1d1ac120acfb4a8469e712628742c
ParkerHansen7777 <parkerhansen7777@gmail.com>
2024-06-20 10:46:07 -0700

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,8 +15,8 @@ render(){
                 <div class='flex-item'>              
                     <img src ={profile} alt="of Parker"/>
                     <p>I'm a software developer based in Los Angeles, California. I have experience with languages and frameworks such as, C, C++, Python, JavaScript, Java, React.JS, Node.JS, SQL. 
-                        I'm currently freelancing, training AI with a focus on software development. I've created multiple projects React with some being full stack, I've even made a Minecraft mod with Java!. 
-                        I love to problem solve. </p>
+                        I'm currently freelancing, training AI with a focus on software development. I've created multiple projects with React with some using the MERN stack, I've even made a Minecraft mod with Java!. 
+                        I love to problem solve. I love to build things. I'm passionate about programming with how solving smaller problems leads to a solution to a big problem, thus building something great! </p>
                 </div>    
                 <div class='flex-item'>    
                     <button class="button1"><a href = "https://github.com/ParkerHansen7777?tab=repositories" target="_blank" rel="noreferrer">GitHub</a></button>
