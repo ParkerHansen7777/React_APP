160c27dbc48f4ed3acfe7d3043c20d451591a18d
ParkerHansen7777 <parkerhansen7777@gmail.com>
2024-09-20 09:31:59 -0700

    Added project pictures to project showcase

--- src/Npp.js
+++ src/Npp.js
@@ -2,6 +2,9 @@ import React from 'react';
 //import NavBar from './components/NavBar/NavBar';
 import './Npp.css';
 import profile from './components/Home/linkedinPfp.jpg';
+import tmPic from './pictures/task-manager.png'
+import laPic from './pictures/luxury-autos.png'
+import ctPic from './pictures/chess-tracker.png'
 export default function Npp(){
 
     return(
@@ -25,22 +28,23 @@ export default function Npp(){
             <section>
                 <h1>Projects i've worked on</h1>
                 <div className='container-row'>
-                <img />
-                <ul>
-                    <li><a>im a link</a></li>
-                </ul>
-                <img />
-                <ul>
-                    <li><a>im a link</a></li>
-                </ul>
-                <img />
-                <ul>
-                    <li><a>im a link</a></li>
-                </ul>
-                <img />
-                <ul>
-                    <li><a>im a link</a></li>
-                </ul>
+                    <h2>Chess Game Tracker</h2>
+                    <img src={ctPic} alt="Chess Game Tracker"/>
+                    <ul>
+                        <li><a>im a link</a></li>
+                    </ul>
+                    <h2>Task Manager</h2>
+                    <img src={tmPic} alt="Task Manager"/>
+                    <ul>
+                        <li><a>im a link</a></li>
+                    </ul>
+                    <h2>Luxury Autos Car Dealership Catalog</h2>
+                    <img src={laPic} alt="Luxury Autos Car Dealership Catalog"/>
+                    <ul>
+                        <li><a>im a link</a></li>
+                    </ul>
+                    <h2>Minecraft: Pizza Mod</h2>
+                    <img alt="Minecraft: Pizza Mod"/>
                 </div>
                 <a href = "https://github.com/ParkerHansen7777?tab=repositories" target="_blank" rel="noreferrer">Github</a>
                 <a href = "https://www.linkedin.com/in/parkerhansen/" target="_blank"rel="noreferrer">LinkedIn</a>
