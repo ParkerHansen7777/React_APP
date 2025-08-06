4495e595413d0a73fc0b54d2ab680139dde782e6
ParkerHansen7777 <parkerhansen7777@gmail.com>
2025-08-05 19:58:11 -0700

    Update Landing.js

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Landing(){
     return(
         <section>
-            <h1>Hello, my name is Parker Hansen....</h1>
+            <h1>Hello, my name is Parker Hansen...</h1>
             <span>Scroll down</span>
             <span>\/</span>
         </section>
